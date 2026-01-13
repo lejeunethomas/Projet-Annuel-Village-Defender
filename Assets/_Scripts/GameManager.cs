82ab5e75f592b920271afc26815e5fd04f75472e
robzz91 <robin.paschini@gmail.com>
2026-01-14 00:40:36 +0100

    health bar

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance; // Singleton pour accès facile
+
+    public int gold = 100;
+    public int baseHealth = 10;
+    public BaseHealthUI baseHealthUI;
+
+    void Awake()
+    {
+        Instance = this;
+        if (baseHealthUI != null)
+            baseHealthUI.UpdateHealth(baseHealth);
+    }
+
+    public void AddGold(int amount)
+    {
+        gold += amount;
+        Debug.Log("Gold: " + gold);
+    }
+
+    public void DamageBase(int amount)
+    {
+        baseHealth -= amount;
+        Debug.Log("Base HP: " + baseHealth);
+        if (baseHealthUI != null)
+            baseHealthUI.UpdateHealth(baseHealth);
+
+        if (baseHealth <= 0)
+        {
+            Debug.Log("GAME OVER");
+            // Recharge la scène pour recommencer (simple pour le prototype)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
